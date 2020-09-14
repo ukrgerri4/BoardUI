949b94793557879c2ef6ae23b27fc857acdc1a10
ukrgerri4 <ukrgerri4@gmail.com>
2020-09-14 23:13:50 +0300

    fix

--- src/app/modules/board/modules/resistance/components/in-game/resistance-in-game.component.ts
+++ src/app/modules/board/modules/resistance/components/in-game/resistance-in-game.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ResistanceSignalRService } from '../../services/resistance-signalr.service';
+import { Subject } from 'rxjs';
+import { takeUntil, filter } from 'rxjs/operators';
+import { ResistanceMessage, ResistanceSignalRService } from '../../services/resistance-signalr.service';
 
 @Component({
   selector: 'app-resistance-in-game',
@@ -12,6 +14,8 @@ export class ResistanceInGameComponent implements OnInit {
 
   private gameId: string = null;
 
+  private destroy$: Subject<boolean> = new Subject<boolean>();
+
   constructor(
     private route: ActivatedRoute,
     private resistanceSignalRService: ResistanceSignalRService
@@ -19,5 +23,20 @@ export class ResistanceInGameComponent implements OnInit {
 
   ngOnInit(): void {
     this.gameId = this.route.snapshot.paramMap.get('id');
+    this.initMessagesSubscription();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
+
+  initMessagesSubscription() {
+    this.resistanceSignalRService.onMessage
+      .pipe(
+        takeUntil(this.destroy$),
+        filter(event => event?.messageType === ResistanceMessage.GameState)
+      )
+      .subscribe(event => console.log(event));
   }
 }
